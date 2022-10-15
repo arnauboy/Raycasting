e440ff3437c5586cd05d53080212e673bb2acfc5
ArnaudMegaDude <arnauboy@gmail.com>
2022-10-15 22:11:05 +0200

    added particles system and force when shooting

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -7,8 +7,11 @@ public class Gun : MonoBehaviour
 
     public float damage = 10f;
     public float range = 100f;
+    public float impactForce = 100f;
 
     public Camera fpsCam;
+    public ParticleSystem flash;
+    public GameObject impactEffect;
     // Update is called once per frame
     void Update()
     {
@@ -19,14 +22,19 @@ public class Gun : MonoBehaviour
     }
 
     void Shoot() {
+        flash.Play();
         RaycastHit hit;
         if (Physics.Raycast(fpsCam.transform.position,fpsCam.transform.forward, out hit, range))
         {
-            Debug.Log(hit.transform.name);
             Target target = hit.transform.GetComponent<Target>();
             if (target!=null) {
                 target.TakeDamage(damage);
             }
+            if(hit.rigidbody != null) {
+                hit.rigidbody.AddForce(-hit.normal * impactForce);
+            }
+            GameObject impactGO = Instantiate(impactEffect, hit.point, Quaternion.LookRotation(hit.normal));
+            Destroy(impactGO, 2f); 
         }
     }
 }
